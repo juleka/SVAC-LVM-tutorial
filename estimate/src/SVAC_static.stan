a8b73a6d2053e87cfc135ac6c64cd378240f1fd0
juleka <jule@hrdag.org>
2018-12-06 17:15:03 -0500

    ran static model

---
+++ estimate/src/SVAC_static.stan
@@ -0,0 +1,75 @@
+//stan code for static model
+
+data {
+  
+    int n_state;
+    int state[n_state];
+    int index_state[n_state];
+
+    int n_ai;
+    int ai[n_ai];
+    int index_ai[n_ai];
+
+    int n_hrw;
+    int hrw[n_hrw];
+    int index_hrw[n_hrw];
+
+    int n_all;
+}
+
+parameters {
+  
+    //beta is a real number, bounded between zero and infinity, 
+    // there are three intercepts, because we have three variables (AI, HRW, and State)
+    real<lower=0> beta[3];
+
+    //this is a special type of object, technically a real number, preserving the 
+    //  order of the underlying order logit
+    //  set of ordered cutpoints go into the ordered var
+    //  it is a coincidence that there are 3 vars/items with 3 levels/thresholds here
+    //  e.g., adding PTS we would have 4 vars, one of 5 levels
+    //  c... stands for cutpoint here
+    ordered[3] c_state;
+    ordered[3] c_ai;
+    ordered[3] c_hrw;
+
+    //latent trait, there will be a value for every single n we have
+    //   in the static model, if a country has all missing values, there will just be the
+    //   prior because actual value cannot be computed
+    vector[n_all] theta_raw;
+}
+
+transformed parameters {
+  
+    //looping through all of our subscripts, monitoring theta to use for inference
+    vector[n_all] theta;
+
+    //for all observed units, we deterministically transform theta, taking vector draw
+    //   theta goes into the models, not theta_raw
+    //   theta==theta_raw
+    for(ii in 1:n_all){
+        theta[ii] = theta_raw[ii];
+    }
+}
+
+model{
+
+    //theta raw is a vector of normally distributed values
+    theta_raw ~ normal(0, 1);
+
+    //likelihood equations, trying to pick the best cutpoints for theta
+    //   each ordered logit 
+    for(ii in 1:n_state){
+        state[ii] ~ ordered_logistic(beta[1] * theta[index_state[ii]], c_state);
+    }
+
+    for(ii in 1:n_ai){
+        ai[ii] ~ ordered_logistic(beta[2] * theta[index_ai[ii]], c_ai);
+    }
+
+    for(ii in 1:n_hrw){
+        hrw[ii] ~ ordered_logistic(beta[3] * theta[index_hrw[ii]], c_hrw);
+    }
+
+    beta ~ normal(0, 3);
+}
---
+++ estimate/src/estimate-static-SVAC.R
@@ -0,0 +1,117 @@
+## Authors:      CJF
+## Maintainers:  CJF, Ju
+##
+## ---------------------------------
+## Purpose: calculate static SVAC model
+## ---------------------------------
+
+library(argparse)
+library(yaml)
+library(rstan)
+library(loo)
+library(coda)
+
+rm(list=ls())
+
+## declare arguments if working with Makefile
+parser    <- ArgumentParser()
+parser$add_argument("--inputfile", type='character')
+parser$add_argument("--STANcode", type='character')
+parser$add_argument("--CONSTANTS", type='character')
+parser$add_argument("--outputfile", type='character')
+parser$add_argument("--outSTANfull", type='character')
+arguments <- parser$parse_args()
+
+## declare arguments if not working with Makefile
+# setwd("~/git/SVAC-LVM-tutorial/")
+# arguments <- list(inputfile='import/output/SVAC_main.csv',
+#                   STANcode='estimate/src/SVAC_static.stan',
+#                   CONSTANTS='estimate/hand/CONSTANTS.yaml',
+#                   outputfile='estimate/output/SVAC_static_est.csv',
+#                   outSTANfull='estimate/output/SVAC_static_full.rds')
+
+#the CONSTANTS in the yaml file will declare some additional STAN parameters 
+CONSTANTS <- yaml.load_file(arguments$CONSTANTS)
+
+data <- read.csv(arguments$inputfile, header=TRUE, sep='|', stringsAsFactors = FALSE)
+
+## adding +1 because STAN wants categorical data to start at 1 
+state <- data$state_prev + 1
+ai    <- data$ai_prev + 1
+hrw   <- data$hrw_prev + 1
+
+## STAN deals w missing data (NA observations in the prevalence vectors) differently, 
+##  we keep track of missing data by setting them relative to the full index:
+##  i.e., we index the observed values only when they are not missing (e.g., index_state)
+##  while also keeping track of the original index positions with: index_all
+index_all   <- 1:nrow(data)
+index_state <- index_all[!is.na(state)]
+index_ai    <- index_all[!is.na(ai)]
+index_hrw   <- index_all[!is.na(hrw)]
+
+## ensure that the observed vars are of the same length as their indices; 
+##  these are the vectors entering the model
+state <- state[!is.na(state)]
+ai    <- ai[!is.na(ai)]
+hrw   <- hrw[!is.na(hrw)]
+
+## create scalars, i.e., the total number of observed values; 
+## in STAN, we declare the length of each of the vectors, 
+## so the program knows how long they are before entering them in model
+n_state <- length(state)
+n_ai    <- length(ai)
+n_hrw   <- length(hrw)
+n_all   <- nrow(data)
+
+## create a list of all things created so far, to be processed in STAN
+stan.data <- list(
+  n_state = n_state,
+  n_ai    = n_ai,
+  n_hrw   = n_hrw,
+  n_all   = n_all,
+  
+  index_state = index_state,
+  index_ai    = index_ai,
+  index_hrw   = index_hrw,
+  
+  state = state,
+  ai    = ai,
+  hrw   = hrw
+)
+
+## for the sampling procedure, we want to reproduce the exact same results every time we run this model
+##  this way, journals and others are able to reproduce our findings
+##  you can pick a seed at random, set it in the CONSTANTS file
+set.seed(CONSTANTS$random_seed)
+
+#create fitted object to pass into Stan
+static.stan.fit <- stan(
+  file=arguments$STANcode,
+  data=stan.data,
+  ## iter: number of times simulations will happen, the first half are used for 
+  ##   burning in the model and get thrown out, the second half are used for inference and get saved. 
+  ##   To know you have enough iterations, we will calculate the R-hat statistic (Gelman-Rubin statistic) 
+  ##   --> the Rhat statistic has to be as close to 1 possible to ensure that the model has converged
+  iter=CONSTANTS$static_STAN_iter,
+  ## chains: designate the number of independent simulations happening at the same time, 
+  ## each simulation gets computed in parallel by one processor
+  chains=CONSTANTS$static_STAN_chains,
+  ## set the number of cores in the CONSTANTS file based on the technical specifications of your machine, 
+  ##  i.e., do not exceed the number of cores you have available
+  cores=CONSTANTS$static_STAN_cores
+)
+
+#STAN passes the simulated values back to R in the form of a list, putting all the chains into separate arrays
+saveRDS(static.stan.fit, file=arguments$outSTANfull)
+staticstanout <- extract(static.stan.fit)
+
+## staticstanout gives us a list of model parameters 
+##  theta is the latent variable, we want its mean, std, upper and lower bounds for plotting later
+data$theta       <- apply(staticstanout$theta, 2, mean)
+data$theta_sd    <- apply(staticstanout$theta, 2, sd)
+#CREDIBLE intervals (CI)
+data$theta_upper <- apply(staticstanout$theta, 2, quantile, 0.975)
+data$theta_low   <- apply(staticstanout$theta, 2, quantile, 0.025)
+
+write.table(data, arguments$output, sep="|", row.names = FALSE)
+##end of Rscript.
