f6144f30040302aea56c1650b193fdb30f356695
juleka <jule@hrdag.org>
2018-12-10 18:44:00 -0500

    adding selection of model functions, i.e., rhat plot, cutpoints plot function

---
+++ estimate/src/model-functions.R
@@ -0,0 +1,56 @@
+## Authors:      Ju
+## Maintainers:  Ju
+##
+## --------------------------------------------------------------------
+## Purpose: a selection of functions to be used with stan model objects
+## --------------------------------------------------------------------
+
+make_Rhat_plot <- function(stan_data_out, model_name) {
+  
+  ## this function is run on the data object that comes straight
+  ## out of stan
+  
+  output_file_name <- paste('output/gg-rhat-SVAC-', model_name, '.pdf', sep='')
+  print(paste('plotting rhats to' , output_file_name))
+  
+  stan_rhat(stan_data_out, fill='grey', bins=30)
+  ggsave(file=output_file_name, height=8, width=6)
+}
+
+
+plot_cutpoints_by_source <- function(extracted_fit, model_name) {
+  
+  ## this model is run on the object that was extracted from the stan object
+  ## model_name: static, dynamic, static-subset, dynamic-subset
+  
+  source_names <- c('ai', 'hrw', 'state')
+  
+  for (sname in source_names) { 
+    
+    print(paste('Now working on cutpoint plot for:', sname))
+    
+    c_vec   <- paste('c', sname, sep='_')
+    
+    cut_point_obj  <- data.frame(category=rep(1, length(extracted_fit[[c_vec]][,1])), 
+                                 value= (extracted_fit[[c_vec]][,1] / 
+                                           extracted_fit[['beta']][,1]))
+    cut_point_obj  <- rbind(cut_point_obj, data.frame(category=rep(2, length(extracted_fit[[c_vec]][,2])), 
+                                                      value=(extracted_fit[[c_vec]][,2] / 
+                                                               extracted_fit[['beta']][,2])))
+    cut_point_obj  <- rbind(cut_point_obj, data.frame(category=rep(3, length(extracted_fit[[c_vec]][,3])), 
+                                                      value=(extracted_fit[[c_vec]][,3] / 
+                                                               extracted_fit[['beta']][,3])))
+    
+    outputfilename <- paste('output/bp-cp-', model_name, '-', sname, '.pdf', sep='')
+    
+    print(paste('Plotting cut points to', outputfilename))
+    pdf(outputfilename, height=8, width=8)
+    par(las=1)
+    boxplot(value~category, data=cut_point_obj, xlab='Category', ylab='Value')
+    dev.off()
+  }
+}
+
+
+## end of script.
+
