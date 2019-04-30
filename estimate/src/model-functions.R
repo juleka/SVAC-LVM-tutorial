5cd3106de9fe94d23a07119368d594268d9d0e8f
juleka <jule@hrdag.org>
2019-04-30 09:24:14 -0400

    fixing column selection for cut-point plots, enhancing plot formatting

--- estimate/src/model-functions.R
+++ estimate/src/model-functions.R
@@ -29,7 +29,8 @@ plot_cutpoints_by_source <- function(extracted_fit, model_name) {
   ## extracted_fit: name of the object that was extracted from the stan object
   ## model_name: 'static' or 'dynamic', depending on which model is being estimated
   
-  source_names <- c('ai', 'hrw', 'state')
+    source_names <- c('ai', 'hrw', 'state')
+    prev_colors <- c(CONSTANTS$prev_colors[[1]], CONSTANTS$prev_colors[[2]], CONSTANTS$prev_colors[[3]])
   
   for (sname in source_names) { 
     
@@ -39,13 +40,13 @@ plot_cutpoints_by_source <- function(extracted_fit, model_name) {
     
     cut_point_obj  <- data.frame(category=rep(1, length(extracted_fit[[c_vec]][,1])), 
                                  value= (extracted_fit[[c_vec]][,1] / 
-                                           extracted_fit[['beta']][,1]))
+                                           extracted_fit[['beta']][, which(source_names==sname)]))
     cut_point_obj  <- rbind(cut_point_obj, data.frame(category=rep(2, length(extracted_fit[[c_vec]][,2])), 
                                                       value=(extracted_fit[[c_vec]][,2] / 
-                                                               extracted_fit[['beta']][,2])))
+                                                               extracted_fit[['beta']][, which(source_names==sname)])))
     cut_point_obj  <- rbind(cut_point_obj, data.frame(category=rep(3, length(extracted_fit[[c_vec]][,3])), 
                                                       value=(extracted_fit[[c_vec]][,3] / 
-                                                               extracted_fit[['beta']][,3])))
+                                                               extracted_fit[['beta']][, which(source_names==sname)])))
     
     ## code when using Makefile
     outputfilename <- paste('output/bp-cp-', model_name, '-', sname, '.pdf', sep='')
@@ -56,7 +57,9 @@ plot_cutpoints_by_source <- function(extracted_fit, model_name) {
     print(paste('Plotting cut points to', outputfilename))
     pdf(outputfilename, height=8, width=8)
     par(las=1)
-    boxplot(value~category, data=cut_point_obj, xlab='Category', ylab='Value')
+    boxplot(value~category, data=cut_point_obj, xlab='Cut-Point', ylab='Estimated Latent Prevalence',
+            ylim = c(0,7),
+            col=prev_colors[which(source_names==sname)])
     dev.off()
   }
 }
